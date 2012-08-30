bb7070d2c22ad42093350c39968595b9813633a0
Albert Visser <albert@lemoncurry>
2012-08-30 20:39:53 +0200

    configurable backup script for local data

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from fabric.api import local, sudo
 """collection of shortcut functions for common tasks like
 
@@ -19,3 +20,29 @@ def install_scite(version):
     local('sudo cp gscite/*.png /usr/share/scite')
     local('sudo cp gscite/*.jpg /usr/share/scite')
     local('rm gscite -r')
+
+def arcstuff():
+    """backup selected files indicated in an ini file
+    """
+    infile = 'arcstuff.ini'
+    outfile = datetime.datetime.today().strftime('/home/albert/backup_%Y%m%d%H%M%S'
+        '.tar.gz')
+    with open(infile) as f_in:
+        first = True
+        command = 'tar -cIvf {}'.format(outfile)
+        prepend = ''
+        for line in f_in:
+            line = line.strip()
+            if line.startswith('['):
+                if line.endswith(']'):
+                    prepend = line[1:-1]
+                continue
+            if not line or line.startswith('#'):
+                continue
+            try:
+                line, rest = line.split('#', 1)
+            except ValueError:
+                pass
+            path = os.path.join(prepend, line) if prepend else line
+            command = '{} {}'.format(command, path.strip())
+    local(command)
