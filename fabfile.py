11c5f0be1d89766cf9b533a96b2a9e2c4dacb9f9
Albert Visser <albert@lemoncurry>
2012-09-16 16:32:36 +0200

    fab arcstuff commando geschikt voor verschillende configuraties

--- fabfile.py
+++ fabfile.py
@@ -21,28 +21,49 @@ def install_scite(version):
     local('sudo cp gscite/*.jpg /usr/share/scite')
     local('rm gscite -r')
 
-def arcstuff():
-    """backup selected files indicated in an ini file
+def arcstuff(*names):
+    """backup selected files indicated in a .conf file
+
+    if no name(s) provided, select all arcstuff config files present
     """
-    infile = 'arcstuff.ini'
-    outfile = datetime.datetime.today().strftime('/home/albert/backup_%Y%m%d%H%M%S'
-        '.tar.gz')
-    with open(infile) as f_in:
-        first = True
-        command = 'tar -cIvf {}'.format(outfile)
-        prepend = ''
-        for line in f_in:
-            line = line.strip()
-            if line.startswith('['):
-                if line.endswith(']'):
-                    prepend = line[1:-1]
-                continue
-            if not line or line.startswith('#'):
-                continue
-            try:
-                line, rest = line.split('#', 1)
-            except ValueError:
-                pass
-            path = os.path.join(prepend, line) if prepend else line
-            command = '{} {}'.format(command, path.strip())
-    local(command)
+    if not names:
+        files = [x for x in os.listdir(os.path.dirname(__file__))
+            if x.startswith('arcstuff') and os.path.splitext(x)[1] == '.conf']
+        names = []
+        for x in files:
+            y = os.path.splitext(x)[0]
+            z = '' if '_' not in y else y.split('_')[1]
+            names.append(z)
+    else:
+        files = []
+        for name in names:
+            hlp = '_' + name if name else ''
+            fname = 'arcstuff{}.conf'.format(hlp)
+            if not os.path.exists(fname):
+                raise ValueError('abort: no config file for {} ({} does not exist'
+                    ')'.format(name, fname))
+            files.append(fname)
+    for indx, infile in enumerate(files):
+        name = 'all' if not names[indx] else names[indx]
+        dts = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+        outfile = '/home/albert/arcstuff/{}_{}.tar.gz'.format(name, dts)
+        with open(infile) as f_in:
+            first = True
+            command = 'tar -czvf {}'.format(outfile)
+            prepend = ''
+            for line in f_in:
+                line = line.strip()
+                if line.startswith('['):
+                    if line.endswith(']'):
+                        prepend = line[1:-1]
+                    continue
+                if not line or line.startswith('#'):
+                    continue
+                try:
+                    line, rest = line.split('#', 1)
+                except ValueError:
+                    pass
+                path = os.path.join(prepend, line) if prepend else line
+                command = '{} {}'.format(command, path.strip())
+        local(command)
+
