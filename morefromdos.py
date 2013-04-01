80bbe603303625e3cfe3c75a44ce7ecd8a4439ad
Albert Visser <albert@lemoncurry>
2013-04-01 17:24:35 +0200

    added possibility to work on specific extension

--- morefromdos.py
+++ morefromdos.py
@@ -3,12 +3,12 @@ import sys
 import os
 import subprocess as sp
 
-def fromdos(path):
+def fromdos(path, cmp_ext='.py'):
     path = os.path.abspath(path)
     for file in os.listdir(path):
         fullname = os.path.join(path, file)
         name, ext = os.path.splitext(file)
-        if os.path.isfile(fullname) and ext == '.py':
+        if os.path.isfile(fullname) and ext == cmp_ext:
             rc = sp.call(['fromdos', fullname])
             if rc != 0:
                 print('fromdos {} failed!'.format(fullname))
@@ -18,5 +18,7 @@ if __name__ == '__main__':
         fromdos(os.getcwd())
     elif len(sys.argv) == 2:
         fromdos(sys.argv[1])
+    elif len(sys.argv) == 3:
+        fromdos(sys.argv[1], sys.argv[2])
     else:
         print('too many arguments')
