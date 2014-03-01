5e5aebd0261c81fbad300353b61991f5dbe1c607
Albert Visser <albert@lemoncurry>
2014-03-01 22:04:46 +0100

    verbeterde versie sorteerscript

--- sort_file.py
+++ sort_file.py
@@ -1,22 +1,22 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+print('start')
 import sys
 import os
 
 def main(fn):
-    h = os.path.splitext(fn)
-    fnew = '_sorted'.join(h)
-    f = open(fn)
-    regels = f.readlines()
-    f.close()
-    regels.sort()
-    f = open(fnew,"w")
-    for x in regels:
-        f.write(x)
-    f.close()
+    fnew = '_sorted'.join(os.path.splitext(fn))
+    with open(fn) as _in, open(fnew, "w") as _out:
+        regels = _in.readlines()
+        regels.sort()
+        for x in regels:
+            _out.write(x)
+    return fnew
 
 if __name__ == '__main__':
+    print(sys.argv)
     if len(sys.argv) > 1:
-        main(sys.argv[1])
+        fn = main(sys.argv[1])
     else:
-        main("x.txt")
+        fn = main(input("Geef naam van te sorteren file op: "))
+    print("klaar, output in", fn)
