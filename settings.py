24dc130c4996e78704dfb258ed281cc66955f1a3
Albert Visser <albert@lemoncurry>
2015-09-06 12:14:14 +0200

    cssedit repository toegevoegd

--- settings.py
+++ settings.py
@@ -3,14 +3,13 @@ server_root = '/usr/share/nginx/html'
 apache_root = '/var/www'
 
 # repository types
-bb_repos = ['actiereg', 'albums', 'apropos', 'bitbucket', 'doctree',
-    'filefindr', 'hotkeys', 'htmledit', 'logviewer', 'myprojects', 'notetree',
-    'probreg', 'rst2html', 'xmledit']
+cherrypy_repos = ['logviewer', 'rst2html']
+# django repos hebben een andere local root
+django_repos = ['actiereg', 'albums', 'myprojects']
+bb_repos = django_repos + cherrypy_repos + ['apropos', 'bitbucket', 'cssedit',
+    'doctree', 'filefindr', 'hotkeys', 'htmledit', 'notetree', 'probreg', 'xmledit']
 non_bb_repos = ['cobtools', 'jvsdoe', 'leesjcl']
 private_repos = ['bin', 'nginx-config']
 all_repos = bb_repos + private_repos + non_bb_repos
 # repos die geen locale working versie hebben
 non_local_repos = ['absentie', 'doctool', 'magiokis', 'pythoneer']
-# django repos hebben een andere local root
-django_repos = ['actiereg', 'albums', 'myprojects']
-cherrypy_repos = ['logviewer', 'rst2html']
