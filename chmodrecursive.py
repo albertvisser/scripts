94acaeed9774c69e8b1f4c1b1daff9e32c05bf01
Albert Visser <albert@lemoncurry>
2016-08-18 12:34:31 +0200

    aanpassingen ivm overslaan directories en files

--- chmodrecursive.py
+++ chmodrecursive.py
@@ -11,12 +11,25 @@ uit te voeren in de root van de betreffende tree
 
 def doit(path):
     for entry in os.listdir(path):
+        if entry in ('__pycache__', '.hg'):
+            continue
         fnaam = os.path.join(path, entry)
         if os.path.isfile(fnaam):
-            os.chmod(fnaam, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH)
+            try:
+                os.chmod(fnaam, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP |
+                    stat.S_IROTH)
+            except PermissionError:
+                pass
+        elif os.path.islink(fnaam):
+            continue
         elif os.path.isdir(fnaam):
-            os.chmod(fnaam, stat.S_IFDIR | stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-            doit(fnaam)
+            try:
+                os.chmod(fnaam, stat.S_IFDIR | stat.S_IRWXU | stat.S_IRWXG |
+                    stat.S_IRWXO)
+            except PermissionError:
+                pass
+            else:
+                doit(fnaam)
 
 if __name__ == "__main__":
     doit(os.getcwd())
