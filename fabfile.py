10b664aad6f47259043928fc1beebdb8f58afd94
Albert Visser <albert@lemoncurry>
2016-11-07 22:35:52 +0100

    chmodrecursive opgenomen in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import os
+import stat
 import shutil
 import datetime
 from fabric.api import *
@@ -132,6 +133,35 @@ def arcstuff(*names):
                 command = '{} {}'.format(command, path.strip())
         local(command)
 
+def chmodrecursive(path=None):
+    """
+    Permissies in een directory tree op standaard waarden zetten
+    bv. nadat de bestanden zijn gekopieerd vanaf een device
+    dat geen unix permits kan onthouden (zoals een mobiele harddisk)
+
+    eenvoudigst om uit te voeren in de root van de betreffende tree
+    """
+    if path is None: path = os.getcwd()
+    for entry in os.listdir(path):
+        if entry in ('__pycache__', '.hg'):
+            continue
+        fnaam = os.path.join(path, entry)
+        if os.path.isfile(fnaam):
+            try:
+                os.chmod(fnaam, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP |
+                    stat.S_IROTH)
+            except PermissionError:
+                pass
+        elif os.path.islink(fnaam):
+            continue
+        elif os.path.isdir(fnaam):
+            try:
+                os.chmod(fnaam, stat.S_IFDIR | stat.S_IRWXU | stat.S_IRWXG |
+                    stat.S_IRWXO)
+            except PermissionError:
+                pass
+            else:
+                chmodrecursive(fnaam)
 #
 # database server stuff
 #
