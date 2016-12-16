893c11442693c02354f975cdf3f2bb6260a73796
Albert Visser <albert@lemoncurry>
2016-12-16 16:14:38 +0100

    added repos to settings

--- settings.py
+++ settings.py
@@ -7,13 +7,13 @@ projects_base = os.path.expanduser('~/projects')
 # repository types
 cherrypy_repos = ['logviewer', 'rst2html', 'magiokis-cherry']
 django_repos = ['actiereg', 'albums', 'myprojects', 'magiokis-django']
-bb_repos = django_repos[:-1] + cherrypy_repos[:-1] + ['apropos', 'bitbucket',
-    'compare-tool', 'cssedit', 'doctree', 'filefindr', 'hotkeys', 'htmledit',
-    'modreader', 'notetree', 'probreg', 'xmledit']
+fcgi_repos = ['absentie', 'doctool', 'magiokis', 'albums-cgi', 'magiokis-php']
 private_repos = ['bin', 'nginx-config']
-# repos die geen locale working versie hebben
-non_deploy_repos = ['absentie', 'doctool', 'magiokis', 'pythoneer']
-non_bb_repos = [django_repos[-1], cherrypy_repos[-1]] + ['cobtools', 'jvsdoe',
-    'leesjcl', 'magiokis-php'] + non_deploy_repos
+non_web_repos = ['apropos', 'bitbucket', 'compare-tool', 'cssedit', 'doctree',
+    'filefindr', 'hotkeys', 'htmledit', 'modreader', 'notetree', 'probreg',
+    'xmledit']
+non_deploy_repos = ['cobtools', 'jvsdoe', 'leesjcl',  'pythoneer']
+bb_repos = django_repos + cherrypy_repos + non_web_repos + fcgi_repos
+non_bb_repos = non_deploy_repos
 
 all_repos = bb_repos + private_repos + non_bb_repos
