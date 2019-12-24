fe7fdc3d9b8e09d8f13287905b8315c7a3c3a326
Albert Visser <albert.visser@gmail.com>
2019-12-24 09:36:40 +0100

    #783 init afgemaakt en begonnen met de rest (gettext en poedit)

--- lang.py
+++ lang.py
@@ -6,25 +6,33 @@ from invoke import task
 from session import get_project_dir
 
 
-@task(help={'project': 'project to create language support for', 'language': 'language to use',
+def get_base_dir(project):
+    """get project location, if not possible then default to current directory
+    """
+    if project:
+        return get_project_dir(project)
+    return os.getcwd()
+
+
+@task(help={'project': 'project to create language support for',
+            'language': '(comma delimited list of) language(s) to use, if not provided then'
+                        ' en,nl is used',
             'check': 'check only'})
 def init(c, project, check=False, language=''):
-    """create directory structure to add language support for ptoject
+    """create directory structure to add language support for project
     """
     # get project location, cancel if project not known
     base = get_project_dir(project)
-    print('project root according to session.py:', base)
     if not base:
         print('unknown project')
         return
     if language:
-        langs = language.split('.')
+        langs = language.split(',')
     else:
         langs = ['en', 'nl']
-    print(language, langs, check)
-    return
+    # print(project, language, langs, check)
     # create locale subdirectory
-    newpath = os.path,join(base, 'locale')
+    newpath = os.path.join(base, 'locale')
     if not os.path.exists(newpath):
         if check:
             print('language support not present (no `locale` subdirectory)')
@@ -37,38 +45,53 @@ def init(c, project, check=False, language=''):
         if check:
             mld = 'language support already' if test else 'no language support'
             mld += ' present for language `{}`'.format(lang)
-            print(mls)
+            print(mld)
             continue
         if not test:
             os.mkdir(langpath)
         os.mkdir(os.path.join(langpath, 'LC_MESSAGES'))
 
 
-@task(help={'sourcefile': 'file to gather texts from (leave empty to check current directory'})
-def gettext(c, sourcefile=''):
+@task(help={'project': 'project name, if not provided then the current directory is used',
+            'sourcefile': 'file to gather texts from (do not specify to check the entire project'})
+def gettext(c, project='', sourcefile=''):
     """gather strings from file
 
     verzamel gemarkeerde symbolen in het aangegeven source file
     en schrijf ze weg in een bestand genaamd messages.pot
     """
+    base = get_base_dir(project)
+    if not base:
+        print('unknown project')
+        return
     if sourcefile == "":
         sourcefile = '.'
-    c.run("pygettext {}".format(sourcefile))
+    with c.cd(base):
+        c.run("pygettext {}".format(sourcefile))
 
 
-@task(help={'language_code': 'designates language to create tanslation for'})
-def poedit(c, language_code):
+@task(help={'project': 'project name, if not provided then the current directory is used',
+            'language_code': 'designates language to create translation for'})
+def poedit(c, project, language_code):
     """edit translations in language file
 
     bewerkt het aangegeven language file met poedit
     """
+    base = get_base_dir(project)
+    if not base:
+        print('unknown project')
+        return
     fnaam = "{}.po".format(language_code)
-    if not os.path.exists(fnaam):
-        c.run('poedit')
-    else:
-        c.run("poedit {}".format(fnaam))
+    command = 'poedit'
+    if os.path.exists(fnaam):
+        command = ' '.join((command, fnaam))
+    with c.cd(base):
+        c.run(command)
 
 
+# TODO: add project name parameter and change behaviour
+# 'project': 'project name, if not provided then the current directory is used',
+# 'appname': 'name of application to create translation for - defaults to project name ',
 @task(help={'language_code': 'designates language to create tanslation for',
             'appname': 'name of application to create translation for',
             'locatie': 'where to put the language file if not below current directory'})
@@ -91,5 +114,3 @@ def place(c, language_code, appname, locatie=""):
     if not os.path.exists(loc):
         os.mkdir(loc)
     c.run('msgfmt {}.po -o {}'.format(language_code, os.path.join(loc, appname + '.mo')))
-
-
