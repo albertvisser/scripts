ecc55f47e0ddf6661b9451f3729a740ad6eeeba4
Albert Visser <albert.visser@gmail.com>
2021-10-24 10:03:18 +0200

    rekening houden met of/welke utility geïnstalleerd is

--- morefromdos.py
+++ morefromdos.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 """converts all files in a directory from dos (crlf) to linux (lf) line-endings
 
 optionally select only files with a certain extension
@@ -11,21 +11,30 @@ import subprocess as sp
 def fromdos(path, cmp_ext='.py'):
     """apply program "fromdos" to multiple files in a directory
     """
+    breakpoint()
+    for command in ['fromdos', 'dos2unix']:
+        result = sp.run(['which', command])
+        if result.returncode == 0:
+            break
+    else:
+        return 'please install `fromdos` or `dos2unix`'
+    if not cmp_ext.startswith('.'):
+        cmp_ext = '.' + cmp_ext
     path = os.path.abspath(path)
     for file in os.listdir(path):
         fullname = os.path.join(path, file)
         name, ext = os.path.splitext(file)
         if os.path.isfile(fullname) and ext == cmp_ext:
-            rc = sp.call(['fromdos', fullname])
-            if rc != 0:
-                print('fromdos {} failed!'.format(fullname))
+            sp.run([command, fullname])
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        fromdos(os.getcwd())
+        result = fromdos(os.getcwd())
     elif len(sys.argv) == 2:
-        fromdos(sys.argv[1])
+        result = fromdos(sys.argv[1])
     elif len(sys.argv) == 3:
-        fromdos(sys.argv[1], sys.argv[2])
+        result = fromdos(sys.argv[1], sys.argv[2])
     else:
-        print('too many arguments')
+        result = 'too many arguments'
+    if result:
+        print(result)
