c322d5b0d693ac0eed1bb8d6a5671924e6bf0c5a
Albert Visser <albert.visser@gmail.com>
2022-09-17 19:50:11 +0200

    rewritten using argparse, drop 'tmp' argument, added argument for starting column

--- sort_file.py
+++ sort_file.py
@@ -1,39 +1,45 @@
 #! /usr/bin/env python3
 """sort a given (text) file, line by line, alphabetically
 
-depending on the value of the second parameter, the resulting file is either saved
-in the same directory as the original (with the suffix "sorted") or in the system's
-temporary directory
+depending on the value of the "output" parameter, the resulting file is either saved
+in the same directory as the original (with the addition "_sorted") or as the given filename
+added a line position other than the first to start sorting on
 """
 import sys
+import argparse
 import os
-TMP = '/tmp'
-if sys.platform.startswith('win'):
-    TMP = "C:\\Windows\\Tmp"
 
 
-def sort(fn, tmp=False):
-    """sorting the lines of the file and write the result to a new file"""
-    if tmp:
-        fnew = os.path.join(TMP, os.path.basename(fn))
-    else:
-        fnew = '_sorted'.join(os.path.splitext(fn))
-    with open(fn) as _in, open(fnew, "w") as _out:
+def sort(fn, fnew, col):
+    with open(fn) as _in:
         regels = _in.readlines()
+    if col:
+        regels.sort(key=lambda x: x[int(col):])
+    else:
         regels.sort()
+    with open(fnew, "w") as _out:
         for x in regels:
             _out.write(x)
-    return fnew
 
 
 def main(args):
     "check and execute"
-    if len(args) > 1:
-        fn = sort(args[1])
-    else:
-        fn = sort(input("Geef naam van te sorteren file op: "))
-    print("klaar, output in", fn)
+    fn = args.file
+    if not fn:
+        fn = input("Geef naam van te sorteren file op: ")
+    fnew = args.output
+    if not fnew:
+        fnew = '_sorted'.join(os.path.splitext(fn))
+    col = args.column
+    sort(fn, fnew, col)
+    print("klaar, output in", fnew)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    parser = argparse.ArgumentParser(description="Simple file sorting program")
+    parser.add_argument('file', help="Name of file to sort")
+    parser.add_argument('-c', "--column", help="Column from which to start sorting")
+    parser.add_argument('-o', '--output', help=("output filename (default is adding '-sorted' to"
+                                                " the input filename"))
+    args = parser.parse_args()
+    main(args)
--- unittests/test_sort_file.py
+++ unittests/test_sort_file.py
@@ -1,21 +1,27 @@
 import os
+import types
 import pytest
 import builtins
 import sort_file
 
 def test_main(monkeypatch, capsys):
-    "input is een list met minimaal de scriptnaam"
     def mock_sort(*args):
         print('called sort() with args', args)
-        return 'filenaam'
     monkeypatch.setattr(sort_file, 'sort', mock_sort)
-    sort_file.main(['scriptname', 'filename'])
-    assert capsys.readouterr().out == ("called sort() with args ('filename',)\n"
-                                       'klaar, output in filenaam\n')
+    sort_file.main(types.SimpleNamespace(file='filename.ext', output='', column='5'))
+    assert capsys.readouterr().out == ("called sort() with args ('filename.ext',"
+                                       " 'filename_sorted.ext', '5')\n"
+                                       'klaar, output in filename_sorted.ext\n')
+    sort_file.main(types.SimpleNamespace(file='filename', output='', column=5))
+    assert capsys.readouterr().out == ("called sort() with args ('filename', 'filename_sorted', 5)\n"
+                                       'klaar, output in filename_sorted\n')
+    sort_file.main(types.SimpleNamespace(file='filename', output='other_file', column=''))
+    assert capsys.readouterr().out == ("called sort() with args ('filename', 'other_file', '')\n"
+                                       'klaar, output in other_file\n')
     monkeypatch.setattr(builtins, 'input', lambda *args, **kwargs: 'somefile')
-    sort_file.main(['scriptname'])
-    assert capsys.readouterr().out == ("called sort() with args ('somefile',)\n"
-                                       'klaar, output in filenaam\n')
+    sort_file.main(types.SimpleNamespace(file='', output='', column=''))
+    assert capsys.readouterr().out == ("called sort() with args ('somefile', 'somefile_sorted', '')\n"
+                                       'klaar, output in somefile_sorted\n')
 
 def test_sort(monkeypatch, capsys):
     ""
@@ -27,27 +33,22 @@ def test_sort(monkeypatch, capsys):
         os.mkdir(workdir)
     source = os.path.join(workdir, 'test_sort')
     target = os.path.join(workdir, 'test_sort_sorted')
-    target_tmp = '/tmp/test_sort'
     with open(source, 'w') as f:
         print('een regel', file=f)
         print('de eerste', file=f)
         print('volgende', file=f)
         print('ook een', file=f)
         print('dat was het dan', file=f)
-    assert sort_file.sort(source) == target
+    sort_file.sort(source, target, '')
     assert os.path.exists(target)
-    assert not os.path.exists(target_tmp)
     with open(target) as in_:
         data = in_.readlines()
-    os.remove(target)
-
-    assert sort_file.sort(source, tmp=True) == target_tmp
-    assert os.path.exists(target_tmp)
-    assert not os.path.exists(target)
-    with open(target_tmp) as in_:
-        data = in_.readlines()
     assert data == ['dat was het dan\n', 'de eerste\n', 'een regel\n', 'ook een\n', 'volgende\n']
-    os.remove(target_tmp)
-    os.remove(source)
 
-    # sort_file.sort(source, tmp=True)
+    sort_file.sort(source, target, '3')
+    with open(target) as in_:
+        data = in_.readlines()
+    assert data == ['ook een\n', 'een regel\n', 'dat was het dan\n', 'de eerste\n', 'volgende\n']
+    os.remove(source)
+    os.remove(target)
+    os.rmdir(workdir)
